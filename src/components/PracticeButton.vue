1ac63b1058aa7a61e8a6e6108a293153c7ee1c26
ienrum <azang0506@gmail.com>
2023-11-27 17:29:59 +0900

    🌱 : Practice (page)의 버튼 컴포넌트 추가

---
+++ src/components/PracticeButton.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['onClick'],
+});
+</script>
+
+<template>
+  <button class="border-2 border-dashed" @click="$emit('onClick', true)">
+    {{ title }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  border: 2px dashed black;
+  border-radius: 0.375rem;
+
+  padding: 0.5rem 1rem;
+  margin-top: 2rem;
+  &:hover {
+    background-color: #f3f4f6;
+  }
+}
+</style>
