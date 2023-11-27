4944b714633e1240a6bdcb0a14ebb8979b4ee4f8
ienrum <azang0506@gmail.com>
2023-11-27 17:28:16 +0900

    🌱 : Practice (page) header 컴포넌트 추가

---
+++ src/pages/PracticeHeader.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <h1
+    class="flex w-full flex-row justify-center border-2 p-5 font-mono text-xl font-semibold underline decoration-dashed shadow-lg"
+    style="margin-bottom: 4rem">
+    {{ title }}
+  </h1>
+</template>
+
+<style scoped lang="scss">
+@media (max-width: 768px) {
+  h1 {
+    justify-content: start;
+  }
+}
+</style>
