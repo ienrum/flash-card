1990b6cc18ae8ece113f5218d09a3153b8fac7e1
ienrum <azang0506@gmail.com>
2023-11-27 17:30:13 +0900

    🌱 : Practice (page)의 플래시 카드 컴포넌트 추가

---
+++ src/components/PracticeFlashCard.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    question: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="container m-3 flex w-auto justify-center">
+    <div class="flashcard flex flex-col items-center justify-center">
+      <h1
+        class="question rounded-lg border-2 bg-slate-100 p-5 font-mono text-xl font-semibold shadow-lg">
+        {{ question }}
+      </h1>
+      <h1
+        class="answer rounded-lg border-2 bg-slate-100 p-5 font-mono text-xl font-semibold shadow-lg">
+        {{ answer }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container:hover {
+  & .flashcard {
+    transform: rotateY(180deg);
+  }
+}
+.flashcard {
+  transform-style: preserve-3d;
+  transition: all 0.5s ease;
+}
+.question {
+  backface-visibility: hidden;
+}
+.answer {
+  position: absolute;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
+}
+</style>
